ed2e8152179a832e959344a9ca0a19a55e5e2c55
darkmift <darkmift@gmail.com>
2023-03-03 17:51:24 +0200

    reformat,added name field

--- src/store/user.slice.ts
+++ src/store/user.slice.ts
@@ -1,37 +1,39 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit'
-import LocalStorageService from '../Services/LocalStorageService'
-import { Role } from '../types'
+import {PayloadAction, createSlice} from '@reduxjs/toolkit'
+import {Role} from '../types'
 
 type UserState = {
-    isLoggedIn: boolean
-    role: Role
-    kickUser: boolean
+  isLoggedIn: boolean
+  role: Role
+  kickUser: boolean
+  name: string
 }
 
 const initialState: UserState = {
-    isLoggedIn: false,
-    role: 'User',
-    kickUser: false,
+  isLoggedIn: false,
+  role: 'User',
+  kickUser: false,
+  name: 'Guest',
 }
 
 const authSlice = createSlice({
-    name: 'auth',
-    initialState,
-    reducers: {
-        login: (state: UserState, action: PayloadAction<Role>) => {
-            state.isLoggedIn = true
-            state.kickUser = false
-            state.role = action.payload
-        },
-        logout: (state: UserState) => {
-            state.isLoggedIn = false
-            state.kickUser = true
-            LocalStorageService.delete('token')
-            LocalStorageService.delete('role')
-        },
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (
+      state: UserState,
+      action: PayloadAction<{role: Role; name: string}>
+    ) => {
+      state.isLoggedIn = true
+      state.kickUser = false
+      state.role = action.payload.role
+      state.name = action.payload.name
     },
+    logout: (state: UserState) => {
+      state = structuredClone(initialState)
+    },
+  },
 })
 
-export type { Role }
-export const { login, logout } = authSlice.actions
+export type {Role}
+export const {login, logout} = authSlice.actions
 export default authSlice.reducer
