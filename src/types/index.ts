76a6bb3943c3b99ef56564568472784a72a02a66
darkmift <darkmift@gmail.com>
2023-03-03 05:41:42 +0200

    added types

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,8 @@ import React from 'react'
 
 export type Role = 'User' | 'Admin'
 export type LinkConfig = {name: string; pathTo: string}
-export type StyleObject = {[key: string]: string}
+export type GenericObject = {[key: string]: any}
+export type StyleObject = GenericObject
 export type StyleXsObject = {[key: string]: string | number | StyleXsObject}
 export type MuiComponent = (props: any) => JSX.Element
 export type GenericComponent = (props: any) => any
@@ -14,4 +15,36 @@ export type FormData = {
   items?: any
   placeholder?: string
   isDropdown?: boolean
+}
+export type JSONValue =
+  | string
+  | number
+  | boolean
+  | {[x: string]: JSONValue}
+  | Array<JSONValue>
+
+export type Member = {
+  id: string
+  name: string
+  email: string
+}
+
+export type Project = {
+  collectionId: string
+  collectionName: string
+  created: string
+  description: string
+  id: string
+  image: string
+  instructorId: string
+  internshipId: string
+  legacyId: string
+  legacyProjectId: string
+  legacyInstructorId: string
+  legacyInternshipId: string
+  name: string
+  updated: string
+  year: number
+  //might be hydrated
+  members?: Member[]
 }
