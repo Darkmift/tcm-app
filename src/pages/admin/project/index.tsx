54cce8dcefbc7bcda616b76a4366f98d97c8fff3
darkmift <darkmift@gmail.com>
2023-03-03 04:19:38 +0200

    added several forms

--- src/pages/admin/project/index.tsx
+++ src/pages/admin/project/index.tsx
@@ -3,19 +3,28 @@ import Container from '@mui/material/Container'
 import TextField from '@mui/material/TextField'
 import React from 'react'
 import {FormData} from '../../../types'
-import {Box, Typography} from '@mui/material'
+import {TextareaAutosize, Typography} from '@mui/material'
 
 const formItems: FormData[] = [
   {
-    name: 'username',
-    placeholder: 'username',
+    name: 'name',
+    placeholder: 'name',
     Component: TextField,
   },
   {
-    name: 'password',
-    placeholder: 'password',
-    type: 'password',
-    Component: TextField,
+    name: 'description',
+    placeholder: 'description',
+    extraProps: {
+      multiline: true,
+      rows: 4,
+      variant: 'outlined',
+      inputProps: {
+        style: {
+          padding: 0,
+        },
+      },
+    },
+    Component: TextareaAutosize,
   },
 ]
 
@@ -23,10 +32,10 @@ function index() {
   return (
     <Container>
       <Typography sx={{ml: 3, fontWeight: 900, fontSize: 35}} color="primary">
-        LOGIN
+        ADD PROJECT
       </Typography>
 
-      <FormToJson formData={formItems} submitHandler={(data) => {}} />
+      <FormToJson formData={formItems} submitHandler={(data) => {}} submitBtnText={'Add Project'} />
     </Container>
   )
 }
