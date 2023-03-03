7a3bf4e3123a3a84663c1b0c848d6c71260d6a99
darkmift <darkmift@gmail.com>
2023-03-03 17:53:11 +0200

    rewrite method to use consts

--- src/Services/HttpService.ts
+++ src/Services/HttpService.ts
@@ -3,17 +3,15 @@ import LocalStorageService from './LocalStorageService'
 import {Role} from '../types'
 import store from '../store'
 import {logout} from '../store/user.slice'
+import {ROLE_LS_KEY, TOKEN_LS_KEY, USERNAME_LS_KEY} from '@/const'
 
 const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN
-console.log("🚀 ~ file: HttpService.ts:8 ~ DOMAIN:", DOMAIN)
+console.log('🚀 ~ file: HttpService.ts:8 ~ DOMAIN:', DOMAIN)
 const BASE_URL = `${DOMAIN}/api/`
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
 })
-
-const TOKEN_LS_KEY = 'token'
-const ROLE_LS_KEY = 'role'
 
 axiosInstance.interceptors.request.use((config) => {
   const token = LocalStorageService.get(TOKEN_LS_KEY)
@@ -59,17 +57,18 @@ const HttpService = {
         throw new Error('no data!')
       }
 
-      const {token, role} = result.data
-
-      if (token && role) {
-        LocalStorageService.set(TOKEN_LS_KEY, token)
-        LocalStorageService.set(ROLE_LS_KEY, role)
-      } else {
-        throw new Error('Login failed')
-      }
+      const {token, role, username} = result.data
 
       if (role !== 'User' && role !== 'Admin') {
         throw new Error('Role provided not recognized', role)
+      }
+
+      if (!!token && !!username) {
+        LocalStorageService.set(TOKEN_LS_KEY, token)
+        LocalStorageService.set(ROLE_LS_KEY, role)
+        LocalStorageService.set(USERNAME_LS_KEY, username)
+      } else {
+        throw new Error('Login failed')
       }
 
       return role
@@ -77,7 +76,7 @@ const HttpService = {
       console.error('🚀 ~ file: HttpService.ts:17 ~ login ~ error', error)
       return null
     }
-  }
+  },
 }
 
 export default HttpService
