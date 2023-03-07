20494166f536bc4cb01ba0268a4a96efe363be17
darkmift <darkmift@gmail.com>
2023-03-07 16:21:33 +0200

    applied internship display

--- src/pages/year/[year]/internships/[internship].tsx
+++ src/pages/year/[year]/internships/[internship].tsx
@@ -1,8 +1,80 @@
-import React from 'react'
+import styles from '@/styles/Internships.module.css'
+
+import {useAppDispatch, useAppSelector} from '@/store'
+import {setSelectedInternship} from '@/store/internships.slice'
+import {Box, Container, Grid, Typography} from '@mui/material'
+import {useRouter} from 'next/router'
+import React, {useEffect} from 'react'
+import Image from 'next/image'
+import {IMAGE_ASSETS_FOLDER_PATH} from '@/const'
+import ProjectCard from '@/components/ProjectCard'
 
 function Internship() {
+  const router = useRouter()
+  const {
+    query: {internship: internshipId, year},
+  } = router
+  const internshipImagePath = IMAGE_ASSETS_FOLDER_PATH + '/internships/'
+  const projectImagePath = IMAGE_ASSETS_FOLDER_PATH + '/projects/'
+  const dispatch = useAppDispatch()
+  const selectedInternshipRedux = useAppSelector(
+    (state) => state.internships.selectedInternship
+  )
+  const projectsRedux = useAppSelector((state) =>
+    state.projects.projects.filter(
+      (p) => p.internshipId === selectedInternshipRedux?.id
+    )
+  )
+  const selectedYearRedux = useAppSelector((state) => state.years.selectedYear)
+
+  useEffect(() => {
+    if (internshipId?.length) {
+      dispatch(setSelectedInternship(internshipId as string))
+    } else {
+      router.back()
+    }
+  }, [internshipId, router, dispatch])
+
   return (
-    <div>internship</div>
+    <Container>
+      {!!selectedInternshipRedux && (
+        <>
+          <Box className={styles['internships_img']}>
+            <Image
+              src={internshipImagePath + selectedInternshipRedux.image}
+              alt={selectedInternshipRedux.name}
+              width={150}
+              height={80}
+            />
+          </Box>
+          <Typography
+            variant="h6"
+            px={1}
+            sx={{borderLeft: '5px solid #2474e4', marginBottom: '10px'}}
+          >
+            {selectedInternshipRedux.name}
+          </Typography>
+          {projectsRedux.length ? (
+            <Grid container spacing={2} p={3}>
+              {projectsRedux.map((project, key) => (
+                <Grid item key={project.id} xs={12} sm={6} md={4} lg={4}>
+                  <ProjectCard
+                    id={project.id}
+                    title={project.name}
+                    imgName={projectImagePath + project.image}
+                  />
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <Typography>
+              There are no projects associated with{' '}
+              {selectedInternshipRedux.name} in {selectedYearRedux}
+            </Typography>
+          )}
+        </>
+      )}
+    </Container>
   )
 }
 
