a4f4fb043d0102b1e9c9deb0c3d5eff4c31a1a5c
darkmift <darkmift@gmail.com>
2023-03-07 16:22:11 +0200

    reroute if illegal navigation

--- src/pages/year/[year]/projects/[project].tsx
+++ src/pages/year/[year]/projects/[project].tsx
@@ -1,5 +1,5 @@
 import {useRouter} from 'next/router'
-import React from 'react'
+import React, {useEffect} from 'react'
 import {useAppSelector} from '../../../../store'
 import {selectProjectById} from '../../../../store/project.slice'
 import styles from '../../../../styles/ProjectPage.module.css'
@@ -12,25 +12,31 @@ type Props = {}
 function ProjectIndex({}: Props) {
   const router = useRouter()
   const projectId = router.query.project
-  const project = useAppSelector(selectProjectById(projectId as string))
-  const internship = project?.expand?.internshipId
-  const instructor = project?.expand?.instructorId
+  const projectRedux = useAppSelector(selectProjectById(projectId as string))
+  const internship = projectRedux?.expand?.internshipId
+  const instructor = projectRedux?.expand?.instructorId
   // const intenrShip = useAppSelector(selectProjectById(projectId as string))
   // return <div>{project?.id}</div>
   const pathImage = IMAGE_ASSETS_FOLDER_PATH + '/projects/'
+
+  useEffect(() => {
+    if (!!projectRedux?.id === false) {
+      router.back()
+    }
+  }, [projectRedux, router])
   return (
     <>
-      {project && (
+      {projectRedux && (
         <div className={styles['project-container']}>
           <Image
-            src={pathImage + project.image}
-            alt={project.name}
+            src={pathImage + projectRedux.image}
+            alt={projectRedux.name}
             width="400"
             height="480"
           />
           <div className={styles['project-section']}>
-            <Typography>{project.name} </Typography>
-            <p>{project.description} </p>
+            <Typography>{projectRedux.name} </Typography>
+            <p>{projectRedux.description} </p>
             <p>
               <span className={styles['title']}>Internship: </span>
               {internship?.name || 'Unknown'}{' '}
@@ -41,8 +47,8 @@ function ProjectIndex({}: Props) {
             </p>
             <div className={styles['members-container']}>
               <span className={styles['title']}>Members :</span>
-              {project.members &&
-                project.members.map((member) => {
+              {projectRedux.members &&
+                projectRedux.members.map((member) => {
                   return (
                     <div
                       key={member.name}
