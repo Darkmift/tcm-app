1c2a9378d1d01f6e3414573882bdfced5c51f7a2
darkmift <darkmift@gmail.com>
2023-03-09 14:59:41 +0200

    rewrite create/update

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -2,7 +2,7 @@ import {COLLECTIONS} from '@/const'
 import {NextApiRequest, NextApiResponse} from 'next'
 import pocketDbService from '../../../backend/services/pocketbase'
 import checkAuthMiddleware from '../../../backend/middleware/checkIsAdmin'
-import {Project, ProjectMemberRelation, Member} from '@/types'
+import {Project, ProjectMemberRelation, Member, ApiProjectEntry} from '@/types'
 
 // Handler to get all projects
 const getAllProjects = async (
@@ -42,23 +42,57 @@ const getAllProjects = async (
 // Handler to create a new project
 const createProject = async (
   req: NextApiRequest,
-  res: NextApiResponse<Project | {error: string}>
+  res: NextApiResponse<Project | {error: string} | any>
 ) => {
   try {
     // Extract necessary fields from request body
-    const {name, description, year} = req.body
+    const {
+      name,
+      description,
+      year,
+      image,
+      members,
+      instructorId,
+      internshipId,
+    }: ApiProjectEntry = req.body
 
-    if (!name || !description || !year) {
+    if (!name || !description || !year?.year) {
       return res
         .status(400)
         .json({error: 'Please provide a name, description and year'})
     }
 
-    const createdProject = await pocketDbService.insertRecord('projects', {
-      name,
-      description,
-      year,
-    })
+    const createdProject = await pocketDbService.insertRecord(
+      COLLECTIONS.PROJECTS,
+      {
+        name,
+        description,
+        year: year.year,
+        instructorId: instructorId?.id,
+        internshipId: internshipId?.id,
+        imageUrl: image,
+      }
+    )
+
+    if (!createdProject) throw new Error('project creation failed')
+
+    if (members?.length) {
+      for await (const member of members) {
+        const filter = `projectId = "${createdProject.id}" && memberId = "${member.id}"`
+        const result = await pocketDbService.findByFilter(
+          COLLECTIONS.PROJECT_MEMBER_RELATION,
+          filter
+        )
+        if (result) continue
+        await pocketDbService.insertRecord(
+          COLLECTIONS.PROJECT_MEMBER_RELATION,
+          {
+            projectId: createdProject.id,
+            memberId: member.id,
+          }
+        )
+      }
+    }
 
     res.status(201).json(createdProject)
   } catch (error) {
@@ -75,19 +109,89 @@ const updateProject = async (
   res: NextApiResponse<Project | {error: string}>
 ) => {
   try {
-    const {id, name, description, year} = req.body
-
-    if (!id || !name || !description || !year) {
-      return res
-        .status(400)
-        .json({error: 'Please provide an id, name, description and year'})
-    }
-
-    const updatedProject = await pocketDbService.updateRecord('projects', id, {
+    const {
+      id,
       name,
       description,
       year,
-    })
+      image,
+      members,
+      instructorId,
+      internshipId,
+    }: ApiProjectEntry = req.body
+
+    if (!id) {
+      return res.status(400).json({error: 'Please provide an id'})
+    }
+
+    if (!!name?.length === false) {
+      return res.status(400).json({error: 'name provided is invalid'})
+    }
+
+    const rawData: any = {
+      name,
+      description,
+      year,
+      image,
+      members,
+      instructorId,
+      internshipId,
+    }
+
+    for (const key in rawData) {
+      if (!!rawData[key] === false) {
+        delete rawData[key]
+      } else {
+        switch (key) {
+          case 'year':
+            rawData[key] = year.year
+            break
+          case 'instructorId':
+            rawData[key] = instructorId?.id
+            break
+          case 'internshipId':
+            rawData[key] = internshipId?.id
+            break
+        }
+      }
+    }
+
+    const updatedProject = await pocketDbService.updateRecord(
+      'projects',
+      id,
+      rawData
+    )
+
+    const filter = `projectId = "${updatedProject.id}"`
+    const result = await pocketDbService.findByFilter(
+      COLLECTIONS.PROJECT_MEMBER_RELATION,
+      filter
+    )
+
+    for await (const relation of result) {
+      await pocketDbService.deleteRecord(
+        COLLECTIONS.PROJECT_MEMBER_RELATION,
+        relation.id
+      )
+    }
+
+    if (members?.length) {
+      for await (const member of members) {
+        const filter = `projectId = "${updatedProject.id}" && memberId = "${member.id}"`
+        const result = await pocketDbService.findByFilter(
+          COLLECTIONS.PROJECT_MEMBER_RELATION,
+          filter
+        )
+        if (result) continue
+        await pocketDbService.insertRecord(
+          COLLECTIONS.PROJECT_MEMBER_RELATION,
+          {
+            projectId: updatedProject.id,
+            memberId: member.id,
+          }
+        )
+      }
+    }
 
     res.status(200).json(updatedProject)
   } catch (error) {
