6443b6de89e694aff9a250919ccf4452e2725ec7
darkmift <darkmift@gmail.com>
2023-04-30 19:56:44 +0300

    added winnerProject service

--- src/Services/HttpService/WinnerProjectHttpService.ts
+++ src/Services/HttpService/WinnerProjectHttpService.ts
@@ -1,5 +1,7 @@
+import {InsertWinnerProject} from './../../types/WinnerProject.type'
 import {WinnerProject} from '@/types'
 import {axiosInstance} from '.'
+import {AxiosResponse} from 'axios'
 
 const API_WINNER_PROJECTS_URL = '/winner-projects'
 
@@ -8,6 +10,56 @@ const WinnerProjectsHttpService = {
     try {
       const response = await axiosInstance.get(API_WINNER_PROJECTS_URL)
       return response.data.winnerProjects
+    } catch (error: any) {
+      throw new Error(error.response.data.error)
+    }
+  },
+  async addWinnerProject(
+    winnerProject: InsertWinnerProject
+  ): Promise<WinnerProject | null> {
+    try {
+      const result: AxiosResponse<any, any> = await axiosInstance.post(
+        API_WINNER_PROJECTS_URL,
+        winnerProject
+      )
+
+      if (!result?.data) {
+        throw new Error('registration failed')
+      }
+
+      return result.data
+    } catch (error) {
+      console.error('🚀 ~ file: HttpService.ts:17 ~ register ~ error', error)
+      return null
+    }
+  },
+  async updateWinnerProject(WinnerProject: WinnerProject) {
+    try {
+      const result: AxiosResponse<WinnerProject> = await axiosInstance.put(
+        `${API_WINNER_PROJECTS_URL}?id=${WinnerProject.id}`,
+        WinnerProject
+      )
+
+      if (!result?.data) {
+        throw new Error('no data!')
+      }
+
+      return result.data
+    } catch (error: any) {
+      throw new Error(error.response.data.error)
+    }
+  },
+  async deleteWinnerProject(id: string) {
+    try {
+      const result: AxiosResponse<WinnerProject> = await axiosInstance.delete(
+        `${API_WINNER_PROJECTS_URL}?id=${id}`
+      )
+
+      if (!result?.data) {
+        throw new Error('no data!')
+      }
+
+      return result.data
     } catch (error: any) {
       throw new Error(error.response.data.error)
     }
