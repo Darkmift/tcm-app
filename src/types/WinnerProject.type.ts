6335acf4907ac1b07ac097bbfd9cb171116e17d6
darkmift <darkmift@gmail.com>
2023-04-30 19:54:00 +0300

    added type

--- src/types/WinnerProject.type.ts
+++ src/types/WinnerProject.type.ts
@@ -3,6 +3,10 @@ import {Project} from './Project.type'
 export type WinnerProject = {
   expand: {projectId: Project}
   id: string
+  projectId: string
   title: string
   year: string
 }
+
+export interface InsertWinnerProject
+  extends Pick<WinnerProject, 'projectId' | 'title' | 'year'> {}
