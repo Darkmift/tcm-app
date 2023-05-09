d0f810f0a7d59c98766c744781dd4a46cb2f9689
darkmift <darkmift@gmail.com>
2023-05-09 17:01:50 +0300

    weird bug in build related to object.keys

--- src/components/BasicTable.tsx
+++ src/components/BasicTable.tsx
@@ -12,8 +12,18 @@ import React, {useEffect} from 'react'
 
 type Props = {dataset: any[]; customHeaders?: string[]}
 
+function getKeys(obj: any) {
+  if (!obj) return []
+  const k = []
+  for (const key in obj) {
+    k.push(key)
+  }
+  return k
+}
+
 function BasicTable({dataset, customHeaders}: Props) {
-  const headers = customHeaders ? customHeaders : Object.keys(dataset[0])
+  const headers =
+    dataset?.[0] && customHeaders ? customHeaders : getKeys(dataset[0])
 
   useEffect(() => {
     console.log('🚀 ~ file: BasicTable.tsx:17 ~ BasicTable ~ headers:', {
@@ -33,23 +43,24 @@ function BasicTable({dataset, customHeaders}: Props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {dataset.map((row, index) => {
-            return (
-              <TableRow key={index}>
-                {Object.values(row).map((value, jIndex) => {
-                  return (
-                    <TableCell key={jIndex + '-' + index}>
-                      {typeof value === 'string'
-                        ? value
-                        : !!value
-                        ? 'True'
-                        : 'False'}
-                    </TableCell>
-                  )
-                })}
-              </TableRow>
-            )
-          })}
+          {dataset?.length &&
+            dataset.map((row, index) => {
+              return (
+                <TableRow key={index}>
+                  {Object.values(row).map((value, jIndex) => {
+                    return (
+                      <TableCell key={jIndex + '-' + index}>
+                        {typeof value === 'string'
+                          ? value
+                          : !!value
+                          ? 'True'
+                          : 'False'}
+                      </TableCell>
+                    )
+                  })}
+                </TableRow>
+              )
+            })}
         </TableBody>
       </Table>
     </TableContainer>
